228e64713fd4de2a8343374574b9a2ad3b81b6e2
evyatar18 <evyatar38@gmail.com>
2020-04-13 13:00:11 +0300

    edited table view

--- workshop_digitalization/lib/view/table.dart
+++ workshop_digitalization/lib/view/table.dart
@@ -6,36 +6,29 @@ typedef void RowClickListener(
     BasicDataTable instance, Map<String, dynamic> clickedRow);
 
 class BasicDataTable extends StatelessWidget {
-  final List<String> _columns;
-  final String _sortColumn;
-  final bool _ascending;
+  final BasicDataTableListeners listeners;
+  final BasicDataTableData tableData;
 
-  final List<Map<String, dynamic>> _data;
-
-  final List<ColumnClickListener> _columnClickListeners;
-  final List<RowClickListener> _rowClickListeners;
-
-  const BasicDataTable(this._columns, this._sortColumn, this._ascending,
-      this._data, this._columnClickListeners, this._rowClickListeners);
+  const BasicDataTable(this.tableData, this.listeners);
 
   List<DataColumn> _buildColumns() {
-    return _columns.map((name) => DataColumn(
+    return tableData.columns.map((name) => DataColumn(
         label: Text(name),
         numeric: false,
         tooltip: "Sort by $name",
         onSort: (index, ascending) {
-          _columnClickListeners
+          listeners.columnClick
               .forEach((listener) => listener(this, name, ascending));
         }));
   }
 
   List<DataRow> _buildRows() {
-    return _data.map((row) {
+    return tableData.data.map((row) {
       // when tapping on this row
       final onRowTap =
-          () => _rowClickListeners.forEach((listener) => listener(this, row));
+          () => listeners.rowClick.forEach((listener) => listener(this, row));
       return DataRow(
-          cells: _columns.map((col) => DataCell(
+          cells: tableData.columns.map((col) => DataCell(
                 Text(row[col]),
                 onTap: onRowTap,
               )));
@@ -47,8 +40,30 @@ class BasicDataTable extends StatelessWidget {
     return DataTable(
       columns: _buildColumns(),
       rows: _buildRows(),
-      sortAscending: _ascending,
-      sortColumnIndex: _columns.indexOf(_sortColumn),
+      sortAscending: tableData.ascending,
+      sortColumnIndex: tableData.columns.indexOf(tableData.sortColumn),
     );
   }
+}
+
+class BasicDataTableListeners {
+  final columnClick = <ColumnClickListener>[];
+  final rowClick = <RowClickListener>[];
+
+  void addColumnClickListener(ColumnClickListener ccs) => columnClick.add(ccs);
+  void removeColumnClickListener(ColumnClickListener ccs) =>
+      columnClick.remove(ccs);
+
+  void addRowClickListener(RowClickListener rcs) => rowClick.add(rcs);
+  void removeRowClickListener(RowClickListener rcs) => rowClick.remove(rcs);
+}
+
+class BasicDataTableData {
+  final List<String> columns;
+  final String sortColumn;
+  final bool ascending;
+
+  final List<Map<String, dynamic>> data;
+
+  BasicDataTableData(this.columns, this.sortColumn, this.ascending, this.data);
 }
