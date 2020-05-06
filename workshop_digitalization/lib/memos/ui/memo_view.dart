667eee16406032b500b67ac308a8c86e11764199
orizitzer365 <45734207+orizitzer365@users.noreply.github.com>
2020-05-06 19:23:56 +0300

    memo view update

--- workshop_digitalization/lib/memos/ui/memo_view.dart
+++ workshop_digitalization/lib/memos/ui/memo_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:html_editor/html_editor.dart';
+import 'package:intl/intl.dart';
 
 import '../memo.dart';
 
@@ -16,100 +17,160 @@ class MemoView extends StatefulWidget {
 class MemoViewState extends State<MemoView> {
   GlobalKey<HtmlEditorState> keyEditor = GlobalKey();
   GlobalKey<HtmlEditorState> keyTopic = GlobalKey();
+  final _dateFormat = DateFormat('dd/MM/yyyy');
+  final _topicController = TextEditingController();
 
-  final _notesController = TextEditingController();
+  Future<bool> _onWillPop() async {
+    var ch = await _thereIsChanges();
+    if (ch) {
+      return (await showDialog(
+            context: context,
+            builder: (context) => new AlertDialog(
+              title: new Text('Are you sure?'),
+              content: new Text('Do you want to exit without save?'),
+              actions: <Widget>[
+                new FlatButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: new Text('No'),
+                ),
+                new FlatButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: new Text('Yes'),
+                ),
+              ],
+            ),
+          )) ??
+          false;
+    }
+    return true;
+  }
+
   void initState() {
     super.initState();
-    _notesController.text = widget.memo.content;
+    _topicController.text = widget.memo.topic;
   }
 
   @override
   void dispose() {
-    _notesController.dispose();
+    _topicController.dispose();
     super.dispose();
   }
 
-  void _save() {
-    // TODO:: add save logic
+  void _reset() {
+    setState(() {
+      keyEditor.currentState.setText(widget.memo.content);
+      _topicController.text = widget.memo.topic;
+    });
+  }
+
+  Future<bool> _thereIsChanges() async {
+    final txt = await keyEditor.currentState.getText();
+    return (widget.memo.content != txt ||
+        widget.memo.topic != _topicController.text);
+  }
+
+  void _save() async {
+    final txt = await keyEditor.currentState.getText();
+    setState(() {
+      widget.memo.content = txt;
+      widget.memo.topic = _topicController.text;
+    });
+  }
+
+  Widget bottomButtonSection() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ButtonBar(
+        alignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlatButton(
+            color: Colors.blueGrey,
+            onPressed: _reset,
+            child: Text("Reset", style: TextStyle(color: Colors.white)),
+          ),
+          FlatButton(
+            color: Colors.blue,
+            onPressed: _save,
+            child: Text(
+              "Save",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget appBarSection() {
+    return AppBar(
+      title: Text('Edit your Memo'),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: _reset,
+          child: Icon(Icons.refresh),
+        ),
+        FlatButton(
+          onPressed: _save,
+          child: Icon(Icons.save),
+        ),
+      ],
+    );
+  }
+
+  Widget subject() {
+    return ListTile(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text('Subject: '),
+          Expanded(
+            child: TextField(
+              controller: _topicController,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget dates() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Text(
+                'creatad at ${_dateFormat.format(widget.memo.creationDate)}')),
+        widget.memo.lastUpdate != null
+            ? Expanded(
+                child: Text(
+                    'updated at ${_dateFormat.format(widget.memo.lastUpdate)}'))
+            : Container()
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit your Memo'),
-        actions: <Widget>[
-          FlatButton(
-            onPressed: _save,
-            child: Icon(Icons.save),
-          )
-        ],
-      ),
-      body: Card(
-        child: SingleChildScrollView(
-          child: Container(
+    return new WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+          appBar: appBarSection(),
+          body: Card(
             child: Column(
               children: <Widget>[
-                ListTile(
-                    title: Row(
-                  children: <Widget>[
-                    Expanded(child: Text('Subject')),
-                    Expanded(
-                      child: TextField(
-                        controller: _notesController,
-                        onChanged: (text) {
-                          widget.memo.topic = text;
-                        },
-                        cursorColor: Theme.of(context).canvasColor,
-                        decoration: new InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    )
-                  ],
-                )),
+                dates(),
+                subject(),
                 HtmlEditor(
                   hint: "Your text here...",
                   value: widget.memo.content,
                   key: keyEditor,
                   height: 400,
+                  decoration: BoxDecoration(),
                 ),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ButtonBar(
-                      alignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        FlatButton(
-                          color: Colors.blueGrey,
-                          onPressed: () {
-                            setState(() {
-                              //  keyEditor.currentState.setEmpty();
-                              keyEditor.currentState.setText(widget.memo.content);
-                            });
-                          },
-                          child: Text("Reset",
-                              style: TextStyle(color: Colors.white)),
-                        ),
-                        FlatButton(
-                          color: Colors.blue,
-                          onPressed: () async {
-                            final txt = await keyEditor.currentState.getText();
-                            setState(() {
-                              widget.memo.content = txt;
-                            });
-                          },
-                          child: Text(
-                            "Save",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    )),
+                bottomButtonSection(),
               ],
             ),
           ),
-        ),
-      ),
+          resizeToAvoidBottomPadding: false),
     );
   }
 }
