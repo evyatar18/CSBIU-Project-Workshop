1a317f73e79b8fa2dd7619a3bb208d77328ecdb3
evyatar18 <evyatar38@gmail.com>
2020-06-04 13:54:18 +0300

    generalized new student screen to new element screen

--- workshop_digitalization/lib/student_project/student/ui/new_student_view.dart
+++ workshop_digitalization/lib/student_project/student/ui/new_student_view.dart
@@ -1,128 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:workshop_digitalization/global/ui/dialogs.dart';
+import 'package:workshop_digitalization/student_project/element_manager.dart';
 import 'package:workshop_digitalization/student_project/student/student.dart';
+import 'package:workshop_digitalization/student_project/ui/new_element_screen.dart';
 
 import 'student_form.dart';
 
+class StudentElementManager implements ElementManager<Student> {
+  final StudentManager studentManager;
+
+  StudentElementManager(this.studentManager);
+
+  Future<Student> createEmpty() => studentManager.createEmpty();
+
+  Future<void> delete(Student elem) => studentManager.delete(elem);
+  Future<void> save(Student elem) => studentManager.save(elem);
+
+  Student getById(String id) => studentManager.getStudent(id);
+}
+
 class NewStudentScreen extends StatelessWidget {
   final StudentManager studentManager;
-  final _formBuilderKey = GlobalKey<FormBuilderState>();
 
   NewStudentScreen({Key key, @required this.studentManager}) : super(key: key);
 
-  Future<bool> _save(BuildContext context, Student student) async {
-    try {
-      await studentManager.save(student);
-    } catch (e) {
-      await showErrorDialog(
-        context,
-        title: "Error while tried to save student",
-        error: e.toString(),
-      );
-      return false;
-    }
-
-    await showSuccessDialog(context, title: "Saved new student successfully!");
-    return true;
-  }
-
-  Future<bool> _delete(BuildContext context, Student student) async {
-    if (!await showAgreementDialog(
-        context, "Are you sure you want to delete this new student?")) {
-      return false;
-    }
-
-    try {
-      await studentManager.delete(student);
-    } catch (e) {
-      await showErrorDialog(
-        context,
-        title: "Error while tried to delete:",
-        error: e.toString(),
-      );
-      return false;
-    }
-
-    await showSuccessDialog(
-      context,
-      title: "Deleted new student successfully!",
-    );
-    return true;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final studentFuture = studentManager.createEmpty();
-
-    return WillPopScope(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Create New Student'),
-        ),
-        body: SingleChildScrollView(
-            child: FutureBuilder(
-          future: studentFuture,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return CircularProgressIndicator();
-            }
-
-            if (snapshot.hasError) {
-              void snapshotErrorReporter() async {
-                await showErrorDialog(
-                  context,
-                  title: "Error while tried to create new student:",
-                  error: snapshot.error.toString(),
-                );
-                Navigator.pop(context);
-              }
-
-              snapshotErrorReporter();
-            }
-
-            final student = snapshot.data;
-            return Column(
-              children: <Widget>[
-                StudentForm(
-                  student: student,
-                  readOnly: false,
-                  formBuilderKey: _formBuilderKey,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    RaisedButton(
-                      child: Text("Save"),
-                      onPressed: () async {
-                        // set values on student object
-                        _formBuilderKey.currentState.save();
-
-                        // save the student object
-                        if (await _save(context, student)) {
-                          Navigator.pop(context);
-                        }
-                      },
-                    ),
-                    RaisedButton(
-                      child: Text("Delete"),
-                      onPressed: () async {
-                        if (await _delete(context, student)) {
-                          Navigator.pop(context);
-                        }
-                      },
-                    ),
-                  ],
-                )
-              ],
-            );
-          },
-        )),
-        resizeToAvoidBottomPadding: false,
-      ),
-
-      // using back button == deleting
-      onWillPop: () async => await _delete(context, await studentFuture),
+    return NewElementScreen<Student>(
+      elementManager: StudentElementManager(studentManager),
+      elementName: "student",
+      elementFormCreator: ({element, formBuilderKey, readOnly}) {
+        return StudentForm(
+          student: element,
+          formBuilderKey: formBuilderKey,
+          readOnly: readOnly,
+        );
+      },
     );
   }
 }
