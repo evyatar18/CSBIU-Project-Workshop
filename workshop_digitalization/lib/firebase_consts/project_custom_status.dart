cbaddfc979ce9cfb4203b95e1749ce7453bcefbe
evyatar18 <evyatar38@gmail.com>
2020-07-17 15:21:04 +0300

    added custom status

--- workshop_digitalization/lib/firebase_consts/project_custom_status.dart
+++ workshop_digitalization/lib/firebase_consts/project_custom_status.dart
@@ -2,5 +2,5 @@ import 'package:workshop_digitalization/firebase_consts/firebase_root.dart';
 
 extension ProjectStatuses on FirebaseRoot {
   List<String> get statuses => this["project-statuses"].cast<String>();
-  set (List<String> statuses) => this["project-statuses"] = statuses;
+  set statuses(List<String> statuses) => this["project-statuses"] = statuses;
 }
