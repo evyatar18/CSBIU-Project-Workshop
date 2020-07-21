6c25139c9d798f900726a06774bb9ad22a795f9a
evyatar18 <evyatar38@gmail.com>
2020-07-21 17:18:44 +0300

    added update method which supports `bringing back from the dead`

--- workshop_digitalization/lib/global/smart_doc_accessor.dart
+++ workshop_digitalization/lib/global/smart_doc_accessor.dart
@@ -15,13 +15,23 @@ class SmartDocumentAccessor extends DocumentAccessor {
     return deleteWithReference(document.reference);
   }
 
- Future deleteWithReference(DocumentReference reference) async {
-        try {
+  Future deleteWithReference(DocumentReference reference) async {
+    try {
       final m = <String, dynamic>{}
         ..[DEL_PROP] = true
         ..[DEL_TIMESTAMP] = FieldValue.serverTimestamp();
 
       _documentAccessor.updateRaw(m, reference);
+    } on Exception {
+      rethrow;
+    }
+  }
+
+  @override
+  Future update(Document document) {
+    try {
+      final m = Map.of(document.toData())..[DEL_PROP] = false;
+      return _documentAccessor.updateRaw(m, document.reference);
     } on Exception {
       rethrow;
     }
