f00e66700452f35526785bf9ee411bec7bedb439
evyatar18 <evyatar38@gmail.com>
2020-07-22 16:44:00 +0300

    removed useless comments

--- firebase_functions/functions/src/index.ts
+++ firebase_functions/functions/src/index.ts
@@ -1,14 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Henlo from Firebase!");
-// });
-
 admin.initializeApp();
 
 function getDisabled(after: admin.firestore.DocumentSnapshot): boolean {
