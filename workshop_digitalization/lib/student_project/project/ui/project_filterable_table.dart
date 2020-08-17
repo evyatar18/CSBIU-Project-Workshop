3e14cd2425a0c49353e977e24e9a78959197a349
evyatar18 <evyatar38@gmail.com>
2020-08-17 18:34:49 +0300

    added fields to project table

--- workshop_digitalization/lib/student_project/project/ui/project_filterable_table.dart
+++ workshop_digitalization/lib/student_project/project/ui/project_filterable_table.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:workshop_digitalization/filter/filterable.dart';
 import 'package:workshop_digitalization/filter/ui/filterable_field.dart';
+import 'package:workshop_digitalization/person/person.dart';
 import 'package:workshop_digitalization/table/ui/filterable_table.dart';
 
 import '../project.dart';
@@ -48,6 +49,54 @@ final castedSelections = [
   createCastingFilterableField(createSelectionFilterable(numberOfStudents)),
 ];
 
+final domain = ObjectField<Project, String>(
+  name: "domain",
+  getter: (obj) => obj.projectDomain,
+);
+
+final challenges = ObjectField<Project, String>(
+  name: "challenges",
+  getter: (obj) => obj.projectChallenges?.join("; "),
+);
+
+final mentorTechAbility = ObjectField<Project, String>(
+  name: "mentorTechAbility",
+  getter: (obj) => obj.mentorTechAbility,
+);
+
+List<ObjectField<Project, String>> _personFields(
+    String name, Person Function(Project) personGetter) {
+  final personName = ObjectField<Project, String>(
+    name: "$name-name",
+    getter: (proj) {
+      final person = personGetter(proj);
+      if (person == null) {
+        return null;
+      }
+
+      return "${person.firstName} ${person.lastName}";
+    },
+  );
+
+  final personEmail = ObjectField<Project, String>(
+    name: "$name-email",
+    getter: (proj) => personGetter(proj)?.email,
+  );
+
+  final personPhone = ObjectField<Project, String>(
+    name: "$name-phone",
+    getter: (proj) => personGetter(proj)?.phoneNumber,
+  );
+
+  return [personName, personEmail, personPhone];
+}
+
+final _projectPersonFields = [
+  ..._personFields("mentor", (proj) => proj.mentor),
+  ..._personFields("contact", (proj) => proj.contact),
+  ..._personFields("initiator", (proj) => proj.initiator),
+];
+
 Widget createFilterableProjectsTable(Stream<List<Project>> projects,
     void Function(BuildContext, Project) onClick, String title) {
   return FilterableTable<Project>(
@@ -55,7 +104,13 @@ Widget createFilterableProjectsTable(Stream<List<Project>> projects,
     objects: projects,
     textFields: textFields,
     otherFilterables: castedSelections,
-    nonFilterFields: [endDate],
+    nonFilterFields: [
+      endDate,
+      domain,
+      challenges,
+      mentorTechAbility,
+      ..._projectPersonFields
+    ],
     onClick: (context, dynamic proj) => onClick(context, proj as Project),
   );
 }
