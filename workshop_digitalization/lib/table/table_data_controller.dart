b33843e02f2aa1a65159dbe7b64830b57161d5a5
evyatar18 <evyatar38@gmail.com>
2020-08-18 17:21:37 +0300

    filterable table

--- workshop_digitalization/lib/table/table_data_controller.dart
+++ workshop_digitalization/lib/table/table_data_controller.dart
@@ -2,15 +2,22 @@ import 'dart:async';
 
 import 'package:workshop_digitalization/filter/filterable.dart';
 
+/// controls the data in a table
 abstract class TableDataController<T> implements Filterable<T> {
+  /// updates when there is a new data representation
   Stream<TableData<T>> get data;
-  StreamSubscription get dataSubscription;
 
+  /// sorts by a specific column
+  ///
+  /// `columnName` the name of the column
+  /// `ascending` whether the sorting is ascending or descending
   void orderBy(String columnName, bool ascending);
 
+  /// disposes resources
   Future<void> dispose();
 }
 
+/// a data snapshot of the table
 class TableData<T> {
   final List<String> columns;
 
